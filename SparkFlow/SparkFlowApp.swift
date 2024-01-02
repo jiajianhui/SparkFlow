57d16d36925a6bb4769122b8658beed42aa15a3a
jiajianhui <jia15176168273@icloud.com>
2024-01-02 14:50:21 +0800

    Initial Commit

---
+++ SparkFlow/SparkFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkFlowApp.swift
+//  SparkFlow
+//
+//  Created by 贾建辉 on 2024/1/2.
+//
+
+import SwiftUI
+
+@main
+struct SparkFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
